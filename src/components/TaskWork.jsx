1a82046c33e142f7b6d3c38c9e7dde8be0f22c42
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-08 17:11:29 -0600

    Se crean los archivos de lista de trabajos y su css

---
+++ src/components/TaskWork.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export const TaskWOrk = () => {
+    return(
+        <div className='container-works'>
+
+            <div className='container-work'>
+
+            </div>
+
+        </div>
+    )
+}
