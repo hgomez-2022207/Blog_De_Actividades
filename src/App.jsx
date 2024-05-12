e038d0d6e513be1a754a22c98e0bebe0ef55fe38
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-11 23:52:38 -0600

    Se corrige el archivo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useRoutes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import routes from "./routes.jsx";
-import './App.css'
 
 export const App = () => {
   let element = useRoutes(routes);
@@ -11,5 +10,5 @@ export const App = () => {
       {element}
       <Toaster position="bottom-right" reverseOrder={false} />
     </>
-  )
-}
+  );
+};
