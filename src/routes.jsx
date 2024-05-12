bf8da383910f004564b570a62702423ba11f7aeb
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-12 01:37:04 -0600

    Se crea una nueva direccion hacia mas info

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,9 @@
+import { MyAcount } from "./components/my/MyAcouunt.jsx";
 import { Dashboard } from "./pages/dashboard/Dasboard.jsx";
 
 const routes = [
-    {path: '/*', element: <Dashboard />}
+    {path: '/*', element: <Dashboard />},
+    {path: '/my', element: <MyAcount />}
 ]
 
 export default routes;
