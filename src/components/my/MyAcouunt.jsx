8f6e665fcc365c464827bcb92426b2765708db36
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-12 01:38:13 -0600

    se a;aden las carpetas work

---
+++ src/components/my/MyAcouunt.jsx
@@ -0,0 +1,20 @@
+import './MyAcount.css'
+
+export const MyAcount = () => {
+    
+    return (
+        <div className="center-container">
+            <div className="article">
+                <div className='section'>
+                    <div className="conten-info">
+                        <img src="../../../public/img/hgomez.jpg" alt="" class="image conten-info img" />
+                        <h1>more about me</h1>
+                        <p>My name is Herlin Fernando Gomez and I am the owner of this blog, which aims to present several of the projects that I have carried out throughout the year.</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+    )
+}
---
+++ src/components/works/TaskWork.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+export const TaskWOrk = () => {
+    return(
+        <div class="card">
+            <div class="card__content">
+            </div>
+        </div>
+    )
+}
