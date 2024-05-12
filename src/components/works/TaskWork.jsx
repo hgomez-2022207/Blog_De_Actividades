d760987ad1189fc545e6ded29f11d74e718def20
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-12 02:31:02 -0600

    Se realizan cambios en Task

--- src/components/works/TaskWork.jsx
+++ src/components/works/TaskWork.jsx
@@ -2,9 +2,6 @@ import React, { useState } from 'react'
 
 export const TaskWOrk = () => {
     return(
-        <div class="card">
-            <div class="card__content">
-            </div>
-        </div>
+        
     )
 }
