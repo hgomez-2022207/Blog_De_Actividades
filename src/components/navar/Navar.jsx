3dfbe2c633ceff2d46dab2c883bb392860dab512
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-11 23:57:57 -0600

    Se crea el navbar

---
+++ src/components/navar/Navar.jsx
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import './Navbar.css'
+
+const NavButton = ({text, onClickHandler}) => {
+    return(
+        <span className="nav-button" onClick={onClickHandler}>
+            {text}
+        </span>
+    )
+}
+
+export const Navbar = () => {
+
+    const navigate = useNavigate()
+
+    const handleNavigateAboutMe = () => {
+        navigate('/my')
+    }
+
+    const handleNavigateToWorksPage = () => {
+        navigate('/works')
+    }
+
+    const handleNavigateToAddComment = () => {
+        navigate('/add')
+    }
+
+    return(
+        <div className="nav-container">
+                <button className='nav-button' text='' onClick={handleNavigateToAddComment}>Browse</button>
+                <button className='nav-button' text='' onClick={handleNavigateAboutMe}>More about me</button>
+                <button className='nav-button' text='' onClick={handleNavigateToWorksPage}>My works</button>
+        </div>
+    )
+}
