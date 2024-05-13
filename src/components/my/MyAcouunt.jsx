4455434a26ed1f0b4b4f5bf6dc7a66126f92bc2c
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-12 23:42:33 -0600

    Se a;aden imagenes al archivo que habla sobre mi

--- src/components/my/MyAcouunt.jsx
+++ src/components/my/MyAcouunt.jsx
@@ -11,6 +11,15 @@ export const MyAcount = () => {
                         <h1>more about me</h1>
                         <p>My name is Herlin Fernando Gomez and I am the owner of this blog, which aims to present several of the projects that I have carried out throughout the year.</p>
                     </div>
+                    <div className="conten-info">
+                        <h1>My experience in this year</h1>
+                        <div>
+                            <img src="../../../public/img/nodeJs.png" alt="image conten-info img" />
+                            <img src="../../../public/img/react.png" alt="image conten-info img" />
+                            <img src="../../../public/img/Js.png" alt="image conten-info img" />
+                            <img src="../../../public/img/html.png" alt="image conten-info img" />
+                        </div>
+                    </div>
                 </div>
                 
             </div>
