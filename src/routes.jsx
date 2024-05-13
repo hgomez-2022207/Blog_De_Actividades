d0f7cd9e7d126e60221487cd49a0a80d6645e13e
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-12 19:02:58 -0600

    se cambia la direccion de la tabla

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
 import { MyAcount } from "./components/my/MyAcouunt.jsx";
 import { Dashboard } from "./pages/dashboard/Dasboard.jsx";
+import { TaskComments } from './components/comments/TaskComments.jsx'
 
 const routes = [
     {path: '/*', element: <Dashboard />},
-    {path: '/my', element: <MyAcount />}
+    {path: '/my', element: <MyAcount />},
+    {path: '/comments', element: <AddComment />}
 ]
 
 export default routes;
