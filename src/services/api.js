ba1e873671f4ee19f39af6f66ace4406a5f9a9ba
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-12 19:00:07 -0600

    Se crea servicios y el api

--- src/services/api.js
+++ src/services/api.js
@@ -4,3 +4,26 @@ const apiClient = axios.create({
     baseURL: 'http://127.0.0.1:8080/twitch/v1',
     timeout: 5000
 })
+
+export const addComment = async (data) => {
+    try {
+        return await apiClient.post('/comment', data)
+    } catch (e) {
+        return{
+            error: true,
+            e
+        }
+    }
+}
+
+export const getComment = async (comments) => {
+    console.log
+    try {
+        return await apiClient.get('/comment')
+    } catch (e) {
+        return {
+            error: true,
+            e
+        }
+    }
+}
