91bf1cee31f018e1b6b78dc7a9778bbbe41cf7c0
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-12 19:02:04 -0600

    Se crea el archivo addComment y se realizan cmbios a la tabla de comments tanto funcionamiento como css

---
+++ src/components/comments/TaskComments.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import './TaskComments.css'
+
+export const TaskComments = ({comments}) => {
+
+    return(
+        <table className="comment-table">
+            <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>Comment</td>
+                </tr>
+            </thead>
+            <tbody>
+                {comments && comments.length > 0 ? (
+                    comments.map((comment, index) => (
+                    comment.estado !== "false" && (
+                        <tr key={index}>
+                            <td>{comment.name}</td>
+                            <td>{comment.comment}</td>
+                        </tr>
+                    ),
+                    console.log(comments)
+                    ))
+                ) : (
+                    <tr>
+                        <td colSpan="2" className="no-comments">No hay comentarios disponibles</td>
+                    </tr>
+                )}
+                
+            </tbody>
+        </table>
+        
+    )
+}
