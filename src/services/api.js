f9b4132e78c21017bb0e032c9fa8bbe24095422c
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-12 23:43:58 -0600

    Se hacen cambios en servicios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8080/twitch/v1',
+    baseURL: 'http://localhost:8080/apiBlog/v1/',
     timeout: 5000
 })
 
@@ -16,8 +16,7 @@ export const addComment = async (data) => {
     }
 }
 
-export const getComment = async (comments) => {
-    console.log
+export const getComment = async () => {
     try {
         return await apiClient.get('/comment')
     } catch (e) {
