81ac0b34c0c7d6037d759490683df3e64f9467e0
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-14 05:22:19 -0600

    Se corrige dise;o del nav

--- src/components/navar/Navar.jsx
+++ src/components/navar/Navar.jsx
@@ -23,13 +23,13 @@ export const Navbar = () => {
                 <button className='nav-button' text='' onClick={handleNavigateToWorksPage}>Comments</button>
     }*/
 
-    const handleNavigateToAddComment = () => {
-        navigate('/comments')
+    const handleNavigateStart = () => {
+        navigate('/')
     }
 
     return(
         <div className="nav-container">
-                <button className='nav-button' text='' onClick={handleNavigateToAddComment}>Add comment</button>
+                <button className='nav-button' text='' onClick={handleNavigateStart}>Start</button>
                 <button className='nav-button' text='' onClick={handleNavigateAboutMe}>More about me</button>
         </div>
     )
